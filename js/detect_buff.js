48a53c00b19e4f415471078402ae4aed139fde4e
keinoda <kei.noda.46m@st.kyoto-u.ac.jp>
2021-12-02 11:16:25 +0900

    bug fix 固定ダメージの扱いが間違っていたのを修正

--- js/detect_buff.js
+++ js/detect_buff.js
@@ -18,10 +18,10 @@ function detect_randnum(base_damage, fixed_damage, actual_damage) {
         // 小数点以下まで与えられているなら，実際にダメージを再現するか確かめるだけで良い
         for (let idx = 0; idx < rand.length; idx++) {
             var randnum = rand[idx];
-            if (actual_damage == Math.floor((base_damage - fixed_damage) * randnum) + fixed_damage) {
+            if (actual_damage == Math.floor((base_damage - fixed_damage) * randnum + fixed_damage)) {
                 return [0, randnum];
-            } else if (Math.abs(actual_damage - (Math.floor((base_damage - fixed_damage) * randnum) + fixed_damage)) < actual_damage * 0.00001) {
-                return [Math.abs(actual_damage - (Math.floor((base_damage - fixed_damage) * randnum) + fixed_damage)), randnum];
+            } else if (Math.abs(actual_damage - (Math.floor((base_damage - fixed_damage) * randnum + fixed_damage))) < actual_damage * 0.00001) {
+                return [Math.abs(actual_damage - (Math.floor((base_damage - fixed_damage) * randnum + fixed_damage))), randnum];
             }
         }
     } else {
@@ -133,17 +133,18 @@ function detect_buff(base_damage, fixed_damage, actual_damage, CARD_bonus = 100,
         }
     }
     base_damage = (base_damage / ((CARD_bonus_all + CARD_bonus) / 100));
+    base_damage = ((base_damage - fixed_damage) / ((CARD_bonus_all + CARD_bonus) / 100));
     console.log(base_damage);
     for (const sDEF of sDEF_array) {
         for (const ATK_buff of ATK_buff_array) {
             for (const CARD_buff of CARD_buff_array) {
                 for (const s_buff of s_buff_array) {
                     DMG = base_damage
-                        * (CARD_bonus / 100 * (100 + CARD_buff) / 100 + CARD_bonus_all / 100)
+                        * ((CARD_bonus / 100) * ((100 + CARD_buff) / 100) + CARD_bonus_all / 100)
                         * (100 + ATK_buff) / 100 // 攻撃バフ防御デバフ
                         * Math.max((100 + s_buff), 0.1) / 100
                         * Math.max(0, 1.0 - Math.min(5.0, Math.max(0, 1.0 + sDEF / 100) - 1.0)) // 特殊耐性
-                        ;
+                        + fixed_damage;
                     var temp = detect_randnum(DMG, fixed_damage, actual_damage);
                     var temp_randnum = temp[1];
                     var temp_gosa = temp[0];
@@ -152,12 +153,26 @@ function detect_buff(base_damage, fixed_damage, actual_damage, CARD_bonus = 100,
                         cnt += 1;
                     }
                     if (cnt > 100) {
-                        return result;
+                        break;
                     }
                 }
+                if (cnt > 100) {
+                    break;
+                }
+            }
+            if (cnt > 100) {
+                break;
             }
         }
+        if (cnt > 100) {
+            break;
+        }
     }
+    result.sort(function (a, b) {
+        if (a.error < b.error) return -1;
+        if (a.error > b.error) return 1;
+        return 0;
+    });
     console.log(result);
     return result;
 };
@@ -187,11 +202,6 @@ function detect_buff_main() {
         parseFloat(document.detect_buff.randnum.value),
         small_steps
     )
-    result.sort(function (a, b) {
-        if (a.error < b.error) return -1;
-        if (a.error > b.error) return 1;
-        return 0;
-    });
     // var result = detect_buff(54917.5140, 0, 726677, 20, 50, NaN, NaN, NaN);
     var print_result = '';
 
@@ -221,11 +231,13 @@ function rounddown(num, digit) {
     var digitVal = Math.pow(10, digit);
     return (Math.floor(num * digitVal) / digitVal).toFixed(digit);
 }
-
 // detect_buff_main();
-// detect_buff(2640.8646, 0, 3197, 0, 82, NaN, NaN, 1.008);
-// detect_buff(54917.5140, 0, 726677, 20, 50, NaN, NaN, NaN);
-// detect_buff(54917.5140, 0, 129187, 20, 0, 140, 20, NaN);
-// detect_buff(63102.6045, 0, 1376756, 100, 46, NaN, NaN, NaN, [false, false, true, false])
+// detect_buff(2640.8646, 0, 3197, 100, 0, 0, 82, NaN, NaN, 1.008);
+// detect_buff(54917.5140, 0, 726677, 100, 0, 20, 50, NaN, 0, NaN);
+// detect_buff(54917.5140, 0, 129187, 100, 0, 20, 0, 140, 20, NaN);
+// detect_buff(63102.6045, 0, 1376756, 100, 0, 100, 46, NaN, NaN, NaN, [false, false, true, false])
 // detect_buff(64803.4200, 0, 991719, 109.4, NaN, NaN, 0, NaN, [false, false, true, false])
 // detect_buff(5017.6427, 0, 3530, 140, 50, 0, 10, NaN, NaN, 0.993);
+detect_buff(26988.6932, 2886.8, 381952, 150, 50, 206, 203, 666, 75, NaN, [true, true, true, true]);
+detect_buff(30603.9771, 2886.8, 396433, 180, 50, 206, 203, 666, 75, NaN, [true, true, true, true])
+detect_buff(34219.2611, 2886.8, 506306, 210, 50, 206, 203, 671, 75, NaN, [true, true, true, true])
